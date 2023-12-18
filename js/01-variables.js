8d617e57af4aab24603ee6423ab828674bb6e314
Emilse Arias <emilsearias@gmail.com>
2023-12-18 20:27:45 -0300

    agregamos mensajes por consola, documento html y ventana emergente

---
+++ js/01-variables.js
@@ -0,0 +1,17 @@
+// Esto es un comentario de una unica linea
+
+/*
+Esto es un comentario
+de 
+varias lineas
+*/
+
+// mostrar comentarios desde el archivo de js
+console.log('Mensaje por consola');
+console.log("Mensaje por consola");
+
+//mostrar un mensaje en el documento html
+document.write('<p class="text-center text-primary">Primer mensaje en el documento HTML</p>');
+
+//ventanas emergentes
+// alert('Ejemplo de un alerta');
