d6863a8c6ec9529a04bb2635faa40a6fe2d74f45
Emilse Arias <emilsearias@gmail.com>
2024-02-07 21:43:05 -0300

    actualizamos el archivo con el arreglo de juegos, agregando una funcion que se encarge de mostrar el array

--- js/05-arreglos.js
+++ js/05-arreglos.js
@@ -1,9 +1,16 @@
 //crear un array vacio
-
 const listaProductos = [];
-
 //crear un array con datos
 const juegos = ["Stardew valley" ,1 , "fifa", 'league of legends', true, 2024, "fornite"];
+
+const mostrarJuegos = (titulo)=>{
+    document.write(`<h2>${titulo} (${juegos.length})</h2>`)
+    document.write(`<ul>`)
+    for(let posicion=0; posicion < juegos.length; posicion++){
+        document.write(`<li>${juegos[posicion]}</li>`)
+    }
+    document.write(`</ul>`)
+}
 
 //mostrar un array
 console.log(listaProductos);
@@ -15,73 +22,33 @@ document.write(`<p>El array Juegos tiene ${juegos.length} elementos</p>`);
 document.write(`<p>El juego número 3 es ${juegos[2]}</p>`);
 document.write(`<p>El juego número 1 es ${juegos[posicion]}</p>`);
 document.write(`<p>El juego número 20 es ${juegos[20]}</p>`);
-
-document.write(`<h2>Array de juegos 🎮 (${juegos.length})</h2>`)
-document.write(`<ul>`)
-for(let posicion=0; posicion < juegos.length; posicion++){
-    document.write(`<li>${juegos[posicion]}</li>`)
-}
-document.write(`</ul>`)
+mostrarJuegos('Array de juegos');
 
 //agregar elementos en el array
 juegos.unshift('Dark soul', 'Dota');
-document.write(`<h2>Nuevo juego al principio del array 🎮 (${juegos.length})</h2>`)
-document.write(`<ul>`)
-for(let posicion=0; posicion < juegos.length; posicion++){
-    document.write(`<li>${juegos[posicion]}</li>`)
-}
-document.write(`</ul>`)
+mostrarJuegos('Nuevo juego al principio del array 🎮')
+
 
 juegos.push('Mario Bros');
-document.write(`<h2>Nuevo juego al final del array 🎮 (${juegos.length})</h2>`)
-document.write(`<ul>`)
-for(let posicion=0; posicion < juegos.length; posicion++){
-    document.write(`<li>${juegos[posicion]}</li>`)
-}
-document.write(`</ul>`)
+mostrarJuegos('Nuevo juego al final del array 🎮');
 
 juegos.splice(6, 0, 'Terraria');
-document.write(`<h2>Nuevo juego en la posicion 6 del array 🎮 (${juegos.length})</h2>`)
-document.write(`<ul>`)
-for(let posicion=0; posicion < juegos.length; posicion++){
-    document.write(`<li>${juegos[posicion]}</li>`)
-}
-document.write(`</ul>`)
+mostrarJuegos('Nuevo juego en la posicion 6 del array 🎮')
 
 //modificar elementos del array
 juegos[3] = 'Dont starve';
 // juegos = 'Dont starve'; este codigo es incorrecto, no puedo cambiar el contenido total del array por otro valor
-
-document.write(`<h2>Modificamos el juego de la posicion 6 del array 🎮 (${juegos.length})</h2>`)
-document.write(`<ul>`)
-for(let posicion=0; posicion < juegos.length; posicion++){
-    document.write(`<li>${juegos[posicion]}</li>`)
-}
-document.write(`</ul>`)
+mostrarJuegos('Modificamos el juego de la posicion 6 del array 🎮')
 
 //Eliminar elementos del array
 juegos.shift();
-document.write(`<h2>Borramos el primer elemento del array 🎮 (${juegos.length})</h2>`)
-document.write(`<ul>`)
-for(let posicion=0; posicion < juegos.length; posicion++){
-    document.write(`<li>${juegos[posicion]}</li>`)
-}
-document.write(`</ul>`)
+mostrarJuegos('Borramos el primer elemento del array 🎮')
 
 juegos.splice(3,1);
 // juegos.splice(3,2); //borramos desde la posicion 3, la cantidad de 2 elementos
 // juegos.splice(3); // borramos desde la posicion 3 el resto de los elemenos del array
-document.write(`<h2>Borramos el elemento de la posicion 3 del array 🎮 (${juegos.length})</h2>`)
-document.write(`<ul>`)
-for(let posicion=0; posicion < juegos.length; posicion++){
-    document.write(`<li>${juegos[posicion]}</li>`)
-}
-document.write(`</ul>`)
+mostrarJuegos('Borramos el elemento de la posicion 3 del array 🎮')
 
 juegos.pop();
-document.write(`<h2>Borramos el ultimo elemento del array 🎮 (${juegos.length})</h2>`)
-document.write(`<ul>`)
-for(let posicion=0; posicion < juegos.length; posicion++){
-    document.write(`<li>${juegos[posicion]}</li>`)
-}
-document.write(`</ul>`)
+mostrarJuegos('Borramos el ultimo elemento del array 🎮')
+
