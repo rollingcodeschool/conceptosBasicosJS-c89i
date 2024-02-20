ee969e532aac750107110f4c60e9d5a9f29fb5ee
Emilse Arias <emilsearias@gmail.com>
2024-02-19 21:02:27 -0300

    agregamos un ejemplo para mostrar , modificar y eliminar las propiedades del objeto. Tambien usamos los metodos del mismo

--- js/08-objetos.js
+++ js/08-objetos.js
@@ -1,5 +1,6 @@
 //objeto con notación literal
 
+
 const pelicula = {
     //propiedades
     nombre : 'Iron Man',
@@ -10,12 +11,48 @@ const pelicula = {
     director: 'Jon Favreau',
     //metodos
     reproducir: function (){
-        document.write(`<p>La peli se esta reproduciendo... ▶️ </p>`)
+        console.log(this)
+        document.write(`<p>La peli ${this.nombre} se esta reproduciendo... ▶️ </p>`)
     },  
     pausar: () =>{
+        console.log(this)
         document.write(`<p>La peli esta en pausa ⏸️ </p>`)
     },
     stop: function () {
         document.write(`<p>La peli se detuvo ⏹️ </p>`)
     }
 }
+
+//mostrar el objeto
+console.log(pelicula)
+document.write(`<p>Pelicula: ${pelicula.nombre}</p>`)
+document.write(`<p>Genero: ${pelicula.genero}</p>`)
+document.write(`<p>Director: ${pelicula['director']}</p>`)
+document.write(`<p>Director: ${pelicula['Director']}</p>`)
+
+//modificar objeto
+pelicula.nombre = "Ironman 3";
+document.write(`<p>Pelicula: ${pelicula.nombre}</p>`)
+
+//agregar propiedades nuevas
+document.write(`<p>Calidad: ${pelicula.calidad}</p>`);
+pelicula.calidad = 'HD';
+document.write(`<p>Calidad: ${pelicula.calidad}</p>`);
+
+pelicula.calidad = 'Full HD';
+console.log(pelicula);
+
+//borrar una propiedad del objeto
+delete pelicula.calidad;
+console.log(pelicula);
+
+//usar los métodos del objeto
+pelicula.reproducir();
+
+pelicula.pausar();
+
+pelicula.stop();
+
+console.log(pelicula.hasOwnProperty('calidad'))
+console.log(pelicula.hasOwnProperty('anio'))
+
